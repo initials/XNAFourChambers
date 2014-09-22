c470e04024b52339b25da075c12b73124a915666
Shane Brouwer <shane@findesign.com.au>
2014-09-22 17:00:01 +1000

    multi player stuff

---
+++ XNAFourChambers/XNAFourChambers/FourChambers/Levels/MultiPlayerCharacterSelect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using org.flixel;
+
+using System.Linq;
+using System.Xml.Linq;
+
+namespace FourChambers
+{
+    public class MultiPlayerCharacterSelect : FlxState
+    {
+
+        override public void create()
+        {
+            base.create();
+
+            //FourChambers_Globals.MUSIC_LEVEL11;
+
+
+
+
+        }
+
+        override public void update()
+        {
+
+
+
+
+            base.update();
+        }
+
+
+    }
+}
