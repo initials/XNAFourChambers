26ece353aa23d1206c830149ecd6183dc1af0903
Shane Brouwer <shane@findesign.com.au>
2017-01-18 17:54:23 +1100

    x

--- XNAFourChambers/XNAFourChambers/FourChambers/SingleScreen/LevelTiles.cs
+++ XNAFourChambers/XNAFourChambers/FourChambers/SingleScreen/LevelTiles.cs
@@ -13,7 +13,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace FourChambers
 {
-    class LevelTiles : FlxTilemap
+    class LevelTiles : FlxGroup
     {
         private Dictionary<string, string> levelAttrs;
         private FlxTilemap bgTiles;
