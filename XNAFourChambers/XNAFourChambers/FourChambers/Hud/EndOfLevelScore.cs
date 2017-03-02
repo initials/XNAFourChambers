2fbeac19ab3c6f829b04bfabeeb0bf8321ef4b3a
Initials Video Games <shane.brouwer@findesigneffects.local>
2017-03-02 17:22:25 +1100

    x

---
+++ XNAFourChambers/XNAFourChambers/FourChambers/Hud/EndOfLevelScore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using org.flixel;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using XNATweener;
+
+namespace FourChambers
+{
+    public class EndOfLevelScore : FlxGroup
+    {
+
+        public EndOfLevelScore()
+            : base()
+        {
+
+        }
+
+        public override void update()
+        {
+            base.update();
+        }
+    }
+}
